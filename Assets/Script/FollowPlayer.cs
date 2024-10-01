56be2fa0b95baf960d05e49a89ac3ae1861c8d29
Harous <89517296+nathgarr@users.noreply.github.com>
2024-10-01 06:43:08 +0200

    3

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -7,7 +7,7 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private GameObject player, view;
 
 
-    // hello
+   
 
     // Start is called before the first frame update
     void Start()
