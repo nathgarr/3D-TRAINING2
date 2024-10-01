83f5bd3a1d1fab79a78f912190a90ebfbf30e494
Harous <89517296+nathgarr@users.noreply.github.com>
2024-10-01 13:37:23 +0200

    first commit

---
+++ Assets/Script/Controller/playerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    float speed = 20f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveAndJump();
+    }
+    void MoveAndJump()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxix = Input.GetAxis("Vertical");
+        /*float zAxis = Input.GetAxis("");*/
+        Vector3 pos = transform.position;
+        pos.x = pos.x + (xAxis * speed * Time.deltaTime);
+        pos.y = pos.y + (yAxix * speed * Time.deltaTime);
+        /* pos.z = pos.z + (zAxis * speed * Time.deltaTime);*/
+        transform.position = pos;
+    }
+}
--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    [SerializeField] private GameObject player, view;
-    float speed = 20f;
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        MoveAndJump();
-    }
-    void MoveAndJump()
-    {
-
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxix = Input.GetAxis("Vertical");
-    /*    float zAxis = Input.GetAxis("");*/
-        Vector3 pos = transform.position;
-        pos.x = pos.x + (xAxis * speed * Time.deltaTime);
-        pos.y = pos.y + (yAxix * speed * Time.deltaTime);
-       /* pos.z = pos.z + (zAxis * speed * Time.deltaTime);*/
-        transform.position = pos;
-    }
+    [SerializeField] GameObject player, view;
 }
