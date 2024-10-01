6c40b2d3f91cd990c925f1025332ffa994e98b79
Harous <89517296+nathgarr@users.noreply.github.com>
2024-10-01 08:16:08 +0200

    v5

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject player, view;
+    float speed = 20f;
 
 
-   
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,18 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MoveAndJump();
+    }
+    void MoveAndJump()
+    {
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxix = Input.GetAxis("Vertical");
+    /*    float zAxis = Input.GetAxis("");*/
+        Vector3 pos = transform.position;
+        pos.x = pos.x + (xAxis * speed * Time.deltaTime);
+        pos.y = pos.y + (yAxix * speed * Time.deltaTime);
+       /* pos.z = pos.z + (zAxis * speed * Time.deltaTime);*/
+        transform.position = pos;
     }
 }
